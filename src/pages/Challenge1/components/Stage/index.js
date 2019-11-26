29f10bf9c6af771351837b03cf607a4d25faf640
Murilo Henrique <oofleaoo@gmail.com>
2019-11-26 17:54:22 -0300

    Added konva and challenge use it

---
+++ src/pages/Challenge1/components/Stage/index.js
@@ -0,0 +1,135 @@
+import React, { useState } from 'react';
+// import Konva from 'konva';
+import { Stage, Layer, Circle } from 'react-konva';
+import {
+  FaArrowAltCircleDown,
+  FaArrowAltCircleLeft,
+  FaArrowAltCircleRight,
+  FaArrowAltCircleUp,
+} from 'react-icons/fa';
+
+import './index.css';
+
+function StageContainer({ width, height, delta, radius }) {
+  const [circleX, setCircleX] = useState(width / 2);
+  const [circleY, setCircleY] = useState(height / 2);
+
+  const dimensions = {
+    maxX: width - radius,
+    minX: radius,
+    maxY: height - radius,
+    minY: radius,
+  };
+
+  function handleMove(direction, steps) {
+    switch (direction) {
+      case 'Up':
+        if (circleY - steps * delta > dimensions.minY) {
+          setCircleY(circleY - steps * delta);
+        }
+        break;
+      case 'Left':
+        if (circleX - steps * delta > dimensions.minX) {
+          setCircleX(circleX - steps * delta);
+        }
+        break;
+      case 'Right':
+        if (circleX + steps * delta < dimensions.maxX) {
+          setCircleX(circleX + steps * delta);
+        }
+        break;
+      case 'Down':
+        if (circleY + steps * delta < dimensions.maxY) {
+          setCircleY(circleY + steps * delta);
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  // function handleMoveByKeyDown(e) {
+  //   if (e.keyCode === 37) {
+  //     if (circleX-delta > dimensions.minX) {
+  //       setCircleX(circleX - delta);
+  //     }
+  //   } else if (e.keyCode === 38) {
+  //     if (circleY-delta > dimensions.minY) {
+  //       setCircleY(circleY - delta);
+  //     }
+  //   } else if (e.keyCode === 39) {
+  //     if (circleX+delta < dimensions.maxX) {
+  //       setCircleX(circleX + delta);
+  //     }
+  //   } else if (e.keyCode === 40) {
+  //     if (circleY+delta < dimensions.maxY) {
+  //       setCircleY(circleY + delta);
+  //     }
+  //   } else {
+  //     return;
+  //   }
+
+  //   e.preventDefault();
+  // }
+
+  return (
+    <div>
+      <div className="container-top">
+        <div className="container-code">Código</div>
+        <Stage width={width} height={height} className="container-events">
+          <Layer>
+            <Circle
+              x={circleX}
+              y={circleY}
+              radius={radius}
+              fill="#7159c1"
+              stroke="black"
+              strokeWidth={0}
+            />
+          </Layer>
+        </Stage>
+      </div>
+      <div className="container-bottom">
+        <div className="keyboard-container">
+          <div className="buttons-container">
+            <button
+              type="button"
+              className="button"
+              onClick={() => handleMove('Up', 1)}
+            >
+              <FaArrowAltCircleUp size="32" color="white" />
+            </button>
+          </div>
+          <div className="buttons-container">
+            <button
+              type="button"
+              className="button"
+              onClick={() => handleMove('Left', 1)}
+            >
+              <FaArrowAltCircleLeft size="32" color="white" />
+            </button>
+            <button
+              type="button"
+              className="button"
+              onClick={() => handleMove('Down', 1)}
+            >
+              <FaArrowAltCircleDown size="32" color="white" />
+            </button>
+            <button
+              type="button"
+              className="button"
+              onClick={() => handleMove('Right', 1)}
+            >
+              <FaArrowAltCircleRight size="32" color="white" />
+            </button>
+          </div>
+        </div>
+        <div className="tips-container">
+          <p>Estamos dando dicas</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default StageContainer;
--- src/pages/Challenge1/index.js
+++ src/pages/Challenge1/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Page from '~/components/Page';
+import ChallengeContainer from './components/Stage';
 
 export default function Challenge() {
-  return <Page title="Challenge1">Challenge1</Page>;
+  return (
+    <Page title="Challenge1">
+      <ChallengeContainer width={500} height={400} delta={25} radius={30} />
+    </Page>
+  );
 }
