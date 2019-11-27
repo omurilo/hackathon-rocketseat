1db596d2d0be501fb23e2e3012c961cf6d4fd3ad
Murilo Henrique <oofleaoo@gmail.com>
2019-11-27 12:58:07 -0300

    fix physics

--- src/pages/PhysicsChallenge/components/Code/index.js
+++ src/pages/PhysicsChallenge/components/Code/index.js
@@ -5,15 +5,8 @@ import { CodeBox, BoxItem } from './styles';
 export default function Code({ codeBlocks }) {
   return (
     <CodeBox>
-      {codeBlocks.map((codeBlock, idx) => (
-        <BoxItem
-          // eslint-disable-next-line react/no-array-index-key
-          key={`${idx}-${codeBlock.origin}`}
-          origin={codeBlock.origin}
-          onClick={() => codeBlock.setIsOpen(true)}
-        >
-          {codeBlock.string}
-        </BoxItem>
+      {codeBlocks.map(codeBlock => (
+        <BoxItem>{codeBlock}</BoxItem>
       ))}
     </CodeBox>
   );
--- src/pages/PhysicsChallenge/components/Code/styles.js
+++ src/pages/PhysicsChallenge/components/Code/styles.js
@@ -12,9 +12,8 @@ export const BoxItem = styled.li`
   border-radius: 5px;
   margin: 5px;
   padding: 15px;
-  background-color: ${props =>
-    props.origin === 'user' ? '#82cf02' : '#f30092'};
-  color: #fff;
+  background-color: #fff;
+  color: #000;
   box-shadow: 0 0 0 rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
   cursor: pointer;
 `;
